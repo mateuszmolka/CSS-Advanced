7d8421e65038df286ae858f167a5260bdeb1d25c
Unknown <b.0910.molmat@gmail.com>
2017-02-16 11:21:37 +0100

    Nav- modification

--- Materials/PROJECT/js/custom.js
+++ Materials/PROJECT/js/custom.js
@@ -42,6 +42,25 @@ function scrollingFunction(hash){
 });
 //    Function for contact END
 //    --------------------------------------
+//    FUNCTION FOR THE SCROLLING START
+var $document = $(document);
+var threeAndHalf = Number(getComputedStyle(document.body, "").fontSize.match(/(\d*(\.\d*)?)px/)[1]) * 3.5;
+
+  $document.scroll(function() {
+      if ($document.scrollTop() >= threeAndHalf) {
+        $('nav').css({"opacity": 1,
+                     "position": "fixed"
+        });
+        $('nav>ul>.top-menu-item>a').css({"color": "#1ac056"    
+        });
+      } else {
+        $('nav').css({"opacity": 0
+        });
+        $('nav>ul>.top-menu-item>a').css({"color": "white"    
+        });
+      }
+    });
     
     
+//    FUNCTION FOR THE SCROLLING END
 })
