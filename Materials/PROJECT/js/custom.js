e83b7f81680edfca04e557a2064e28ec7f024d4d
Unknown <b.0910.molmat@gmail.com>
2017-02-16 13:55:49 +0100

    Smoth Scroll- mod

--- Materials/PROJECT/js/custom.js
+++ Materials/PROJECT/js/custom.js
@@ -1,48 +1,16 @@
-$(function(){
-    'use strict';
+'use strict';
 //SCROLLING FUNCTION START
-function scrollingFunction(hash){
+function scrollingFunction(element){
       $('html, body').animate({
-        scrollTop: ($(hash).offset().top)-30
+        scrollTop: ($(element.hash).offset().top)-30
       }, 800, function(){
-        window.location.hash = hash;
+        window.location.hash = element.hash;
       });
     }
 //SCROLLING FUNCTION END
-//    --------------------------------------
-//    Function for about me START
-    var menuClickAbout=$('a#top-menu-item-about-me');
-    menuClickAbout.click( function(event) { 
-        event.preventDefault();
-        scrollingFunction(this.hash); 
-    });
-//    Function for about me END
-//    --------------------------------------
-//    Function for about me START
-    var menuClickSkills=$('a#top-menu-item-my-skills');
-    menuClickSkills.click( function(event) { 
-        event.preventDefault();
-        scrollingFunction(this.hash);
-    });
-//    Function for about me END
-//    --------------------------------------
-//    Function for portfolio START
-     var menuClickPort=$('a#top-menu-item-my-portfolio');
-    menuClickPort.click( function(event) { 
-        event.preventDefault();
-        scrollingFunction(this.hash);
-    });
-//    Function for portfolio END
-//    --------------------------------------
-//    Function for contact START
- var menuClickContact=$('a#top-menu-item-my-contact');
-    menuClickContact.click( function(event) { 
-    event.preventDefault();
-    scrollingFunction(this.hash);
-});
-//    Function for contact END
-//    --------------------------------------
-//    FUNCTION FOR THE SCROLLING START
+
+$(function(){
+    'use strict';
 var $document = $(document);
 var threeAndHalf = Number(getComputedStyle(document.body, "").fontSize.match(/(\d*(\.\d*)?)px/)[1]) * 3.5;
 
